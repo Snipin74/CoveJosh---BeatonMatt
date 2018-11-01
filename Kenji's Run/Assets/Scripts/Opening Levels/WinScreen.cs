1ee3351c9f35e70fccd7a7728c5895aedc67561f
mattwbeaton <44066260+mattwbeaton@users.noreply.github.com>
2018-11-01 16:10:01 -0400

    Level Three Layout Finished

---
+++ Kenji's Run/Assets/Scripts/Opening Levels/WinScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScreen : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D ChestCollider)
+    {
+        if (ChestCollider.gameObject.CompareTag("Sword"))
+        {
+
+            Application.LoadLevel(6);
+        }
+    }
+}
